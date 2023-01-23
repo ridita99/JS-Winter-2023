7053a8299cd0cf67a97d165ae5f930db8e9eed2f
ridita malik <riditamalik@riditas-MacBook-Air.local>
2023-01-22 19:25:49 -0500

    Class1code

---
+++ Modules/Class1/File1.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+console.log('everything is beautiful');
+log('Hello');
+
+
+/* let's print out the code. 
+console.log helps to print anything in the terminal 
+*/
---
+++ Modules/Class1/comments.js
@@ -0,0 +1,3 @@
+// anything after the double slash is to be considered as comment
+console.log(12 + 23 + 23);
+// in order to print something you have to type console.log in order to show up in terminal 
