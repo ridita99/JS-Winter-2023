2c50ad292cda58dcb96547213a7d93316140e39b
ridita malik <riditamalik@riditas-Air.lan1>
2023-06-03 14:01:04 -0400

    Practice1

---
+++ class2/Practice1.js
@@ -0,0 +1,32 @@
+let myAge = 20;
+console.log(myAge);
+
+let userName = 'Amina Sarkar';
+let userAge = 20;
+
+console.log(`user Name is: ${userName} and user Age is: ${userAge}years`);
+// if we put\n key then it creates the space
+
+
+let employee100 = {}
+
+let employee101 = {
+    name: 'John Done'
+    //age: 34,
+    //gender: 'M',
+    // salary: 346667
+    //firstJob: 'Manager'
+}
+
+console.log(employee101);
+console.log(typeof employee101);
+console.log(employee101.firstJob);
+
+
+
+// F -> c
+// T(c) = T ((F)-32)/1.8
+
+let ftemp = 47
+let ftempIntoCTemp = (ftemp - 32) / 1.8
+console.log(`\n${ftemp}F is equals to ${ftempIntoCTemp}C`); 
